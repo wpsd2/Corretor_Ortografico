572aba8b4a4ef3d53c51c195c89af9e4b62abc5e
Wanderson <wpsd2@hotmail.com>
2021-08-13 20:49:59 -0300

    test

--- corretor.py
+++ corretor.py
@@ -2,24 +2,23 @@ import nltk as nltk
 
 with open("files/artigos.txt", "r", encoding="utf8") as f:
     artigos = f.read()
-print(artigos[:500])
+#print(artigos[:500])
 
 
 texto_exemplo = "Olá, tudo bem?"
 palavras_separadas = texto_exemplo.split()
 
-
-print(palavras_separadas)
-print(len(palavras_separadas))
+#print(palavras_separadas)
+#print(len(palavras_separadas))
 
 texto_exemplo = "Olá, tudo bem?"
 tokens = texto_exemplo.split()
-print(len(tokens))
-print(tokens)
+#print(len(tokens))
+#print(tokens)
 
-nltk.download('punkt')
+#nltk.download('punkt')
 palavras_separadas = nltk.tokenize.word_tokenize(texto_exemplo)
-print(palavras_separadas)
+#print(palavras_separadas)
 
 
 
@@ -31,14 +30,14 @@ def separa_palavras(lista_tokens):
     return lista_palavras
 
 
-print(separa_palavras(palavras_separadas))
+#print(separa_palavras(palavras_separadas))
 
 
 lista_tokens = nltk.tokenize.word_tokenize(artigos)
 lista_palavras = separa_palavras(lista_tokens)
-print(f"O número de palavras é: {len(lista_palavras)}")
+#print(f"O número de palavras é: {len(lista_palavras)}")
 
-print(lista_palavras[:5])
+#print(lista_palavras[:5])
 
 
 def normalizacao(lista_palavras):
@@ -48,7 +47,7 @@ def normalizacao(lista_palavras):
     return lista_normalizada
 
 lista_normalizada = normalizacao(lista_palavras)
-print(lista_normalizada[:5])
+#print(lista_normalizada[:5])
 
 
 len(set(lista_normalizada))
@@ -57,8 +56,6 @@ len(set(lista_normalizada))
 lista = "lgica"
 (lista[:1], lista[1:])
 palavra_exemplo = 'lgica'
-
-
 
 
 
@@ -81,7 +78,7 @@ def gerador_palavras(palavra):
     return palavras_geradas
 
 palavras_geradas = gerador_palavras(palavra_exemplo)
-print(palavras_geradas)
+#print(palavras_geradas)
 
 
 def corretor(palavra):
@@ -98,9 +95,26 @@ def probabilidade(palavra_gerada):
     return frequencia[palavra_gerada]/total_palavras
 
 probabilidade("lógica")
-print(probabilidade("lógica"))
+#print(probabilidade("lógica"))
 
 
 
 corretor(palavra_exemplo)
-print(corretor(palavra_exemplo))
+#print(corretor(palavra_exemplo))
+
+
+def cria_dados_teste(nome_arquivo):
+    lista_palavras_teste = []
+    f = open('files/palavras.txt', "r", encoding='utf-8')
+    for linha in f:
+        correta, errada = linha.split()
+        lista_palavras_teste.append((correta, errada))
+    f.close()
+    return lista_palavras_teste
+
+lista_teste = cria_dados_teste("palavras.txt")
+#print(lista_teste)
+
+
+
+

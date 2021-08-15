386fcb316194896a0f28bc926010a74ca7fa8ec0
Wanderson <wpsd2@hotmail.com>
2021-08-15 12:51:55 -0300

    change letter

--- corretor.py
+++ corretor.py
@@ -152,6 +152,53 @@ palavras_geradas = gerador_palavras(palavra_exemplo)
 avaliador(lista_teste)
 
 
+def insere_letras(fatias):
+    novas_palavras = []
+    letras = 'abcdefghijklmnopqrstuvwxyzàáâãèéêìíîòóôõùúûç'
+    for E, D in fatias:
+        for letra in letras:
+            novas_palavras.append(E + letra + D)
+    return novas_palavras
 
 
+def deletando_caracteres(fatias):
+    novas_palavras = []
+    for E, D in fatias:
+        novas_palavras.append(E + D[1:])
+    return novas_palavras
 
+
+def troca_letra(fatias):
+    novas_palavras = []
+    letras = 'abcdefghijklmnopqrstuvwxyzáàãâéèêíìóòõôúùûç'
+    for E, D in fatias:
+        for letra in letras:
+            novas_palavras.append(E + letra + D[1:])
+    return novas_palavras
+
+
+def inverte_letra(fatias):
+    novas_palavras = []
+    for E, D in fatias:
+        if len(D) > 1:
+            novas_palavras.append(E + D[1] + D[0] + D[2:])
+    return novas_palavras
+
+
+def gerador_palavras(palavra):
+    fatias = []
+    for i in range(len(palavra)+1):
+        fatias.append((palavra[:i],palavra[i:]))
+    palavras_geradas = insere_letras(fatias)
+    palavras_geradas += deletando_caracteres(fatias)
+    palavras_geradas += troca_letra(fatias)
+    palavras_geradas += inverte_letra(fatias)
+    return palavras_geradas
+
+
+palavra_exemplo = "lgóica"
+palavras_geradas = gerador_palavras(palavra_exemplo)
+#print(palavras_geradas)
+
+
+avaliador(lista_teste)
